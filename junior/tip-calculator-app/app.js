1bbf20e2090080db20c006c611884e4ca8ab11cb
Joseph Reyes <jreyes1919@gmail.com>
2024-04-24 14:41:08 -0500

    finished tip calculator app challenge

--- junior/tip-calculator-app/app.js
+++ junior/tip-calculator-app/app.js
@@ -6,15 +6,32 @@ const customTipInput = document.getElementById("custom");
 const peopleInput = document.getElementById("people");
 const tipElement = document.getElementById("tip-total");
 const totalElement = document.getElementById("total-amount");
+const resetBtn = document.getElementById("reset-btn");
+
+// tip value helper function
+function getTipValue() {
+  const selectedRadioBtn = document.querySelector(
+    'input[name="tip-select"]:checked'
+  );
+  if (!selectedRadioBtn && !customTipInput.value) {
+    return 0;
+  } else if (selectedRadioBtn) {
+    return parseFloat(selectedRadioBtn.value);
+  } else {
+    return parseFloat(customTipInput.value) / 100;
+  }
+}
 
 // get bill value
 billInput.addEventListener("input", handleBillValue);
 
 function handleBillValue(e) {
   const billValue = parseFloat(e.target.value);
+  const tipValue = getTipValue();
+  const peopleValue = parseFloat(peopleInput.value) || 1;
 
-  console.log(billValue);
-  calculateTotal(billValue);
+  calculateTotal(billValue, tipValue, peopleValue);
+  calculateTip(billValue, tipValue, peopleValue);
 }
 
 // get tip value
@@ -23,11 +40,12 @@ radioBtns.forEach((radioBtn) => {
 });
 
 function handleTipValue(e) {
-  const tipValue = e.target.value;
+  const tipValue = parseFloat(e.target.value);
   const billValue = parseFloat(billInput.value);
-  const peopleValue = peopleInput.value;
-  console.log(tipValue);
+  const peopleValue = parseFloat(peopleInput.value);
+  //   console.log(tipValue);
   calculateTotal(billValue, tipValue, peopleValue);
+  calculateTip(billValue, tipValue, peopleValue);
 }
 
 // handle custom tip input
@@ -39,8 +57,9 @@ function handleCustomTipInput(e) {
   const tipValue = parseFloat(e.target.value) / 100;
   const billValue = parseFloat(billInput.value);
   const peopleValue = peopleInput.value;
-  console.log(`custom tip: ${tipValue}`);
+  //   console.log(`custom tip: ${tipValue}`);
   calculateTotal(billValue, tipValue, peopleValue);
+  calculateTip(billValue, tipValue, peopleValue);
 }
 
 // handle people input
@@ -48,26 +67,33 @@ function handleCustomTipInput(e) {
 peopleInput.addEventListener("input", handlePeopleInput);
 
 function handlePeopleInput(e) {
-  const peopleValue = e.target.value;
-  let tipValue;
-
-  // Check if any radio button is selected
-  const selectedRadioBtn = document.querySelector(
-    'input[name="tip-select"]:checked'
-  );
-  if (selectedRadioBtn) {
-    // If a radio button is selected, use its value as the tip value
-    tipValue = selectedRadioBtn.value;
-  } else {
-    // If no radio button is selected, use the custom input value as the tip value
-    tipValue = parseFloat(customTipInput.value) / 100; // Convert to decimal
-  }
+  const peopleValue = parseFloat(e.target.value);
+  const tipValue = getTipValue();
   const billValue = parseFloat(billInput.value);
-  console.log(peopleValue, tipValue, billValue);
+  //   console.log(peopleValue, tipValue, billValue);
   calculateTotal(billValue, tipValue, peopleValue);
+  calculateTip(billValue, tipValue, peopleValue);
 }
 
 // calculate tip
+function calculateTip(billValue, tipValue = 0, peopleValue = 1) {
+  //   console.log("calculate tip:");
+  //   console.log((billValue * tipValue) / peopleValue);
+  // Fallback values
+  billValue = billValue || 0;
+  tipValue = tipValue || 0;
+  peopleValue = parseFloat(peopleValue) || 1;
+  console.log(billValue, tipValue, peopleValue);
+  if (billValue > 0 && peopleValue > 0 && tipValue === 0) {
+    tipElement.innerHTML = "Don't be lame<br> leave a tip";
+    tipElement.style.fontSize = ".75rem"; // Set a smaller font size
+  } else {
+    tipElement.textContent = `$${((billValue * tipValue) / peopleValue).toFixed(
+      2
+    )}`;
+    tipElement.style.fontSize = "";
+  }
+}
 
 // calculate total
 function calculateTotal(billValue, tipValue = 0, peopleValue = 1) {
@@ -77,4 +103,16 @@ function calculateTotal(billValue, tipValue = 0, peopleValue = 1) {
   } else {
     totalElement.textContent = "$0.00";
   }
+}
+
+// reset button
+resetBtn.addEventListener("click", resetInputs);
+
+function resetInputs() {
+  billInput.value = "";
+  peopleInput.value = "";
+  customTipInput.value = "";
+  radioBtns.forEach((radioBtn) => (radioBtn.checked = false));
+  totalElement.textContent = "$0.00";
+  tipElement.textContent = "$0.00";
 }
