e4f4f072666dbb0276fb9be1365dfbe291484790
Joseph Reyes <jreyes1919@gmail.com>
2024-05-17 12:10:45 -0500

    updated work section
    added infinite scroll
    
    bugs:
       - not a smooth transition to children
       - scrolling to right end skips first card

--- junior/single-page-design-portfolio/app.js
+++ junior/single-page-design-portfolio/app.js
@@ -3,6 +3,7 @@ const arrowButtons = document.querySelectorAll(".arrow-button");
 const workScroller = document.querySelector(".work-scroller");
 const workImages = workScroller.querySelectorAll("img");
 const cardWidth = workImages[0].offsetWidth;
+const scrollerChildren = [...workScroller.children];
 
 // render middle image
 window.addEventListener("load", centerMiddleImage);
@@ -18,6 +19,50 @@ function centerMiddleImage() {
     behavior: "auto",
   });
 }
+
+let cardPerView = Math.round(workScroller.offsetWidth / cardWidth);
+
+// insert copies of the last few cards to beginning of scroller for infinite scrolling
+scrollerChildren
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
+    workScroller.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+//   insert copies of the first few cards to end of scroller for infinite scrolling
+scrollerChildren.slice(0, cardPerView).forEach((card) => {
+  workScroller.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// infinite scroll
+
+const infiniteScroll = () => {
+  if (workScroller.scrollLeft === 0) {
+    workScroller.classList.add("no-transition");
+    workScroller.scrollLeft =
+      workScroller.scrollWidth - 2 * workScroller.offsetWidth;
+    requestAnimationFrame(() => {
+      workScroller.classList.remove("no-transition");
+    });
+  } else if (
+    Math.ceil(workScroller.scrollLeft) ===
+    workScroller.scrollWidth - workScroller.offsetWidth
+  ) {
+    workScroller.classList.add("no-transition");
+    workScroller.scrollLeft =
+      workScroller.offsetWidth + cardWidth * cardPerView;
+    requestAnimationFrame(() => {
+      workScroller.classList.remove("no-transition");
+    });
+  }
+
+  requestAnimationFrame(infiniteScroll);
+};
+
+infiniteScroll();
+
+workScroller.addEventListener("scroll", infiniteScroll);
 
 // arrow button click event listener
 arrowButtons.forEach((arrowButton) => {
