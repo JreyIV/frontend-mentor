e44b4484bd23b5242b2a63e521e3c25b069e3541
Joseph Reyes <jreyes1919@gmail.com>
2024-06-14 22:33:26 -0500

    finished github user search app challenge

--- junior/github-user-search-app/app.js
+++ junior/github-user-search-app/app.js
@@ -1,5 +1,7 @@
 let darkMode = localStorage.getItem("darkMode");
 //********** imports **********/
+const userContainer = document.getElementById("user-container");
+const noResult = document.getElementById("no-results");
 
 // buttons
 const toggleButton = document.getElementById("button-toggle-theme");
@@ -7,7 +9,10 @@ const toggleButton = document.getElementById("button-toggle-theme");
 // themes
 const toggleIcon = document.querySelector(".theme-toggle-icons");
 const toggleText = document.getElementById("theme-toggle-text");
-console.log(toggleText);
+
+// form
+const searchForm = document.getElementById("form");
+const searchInput = document.querySelector('input[type="text"]');
 
 //********** darkmode **********//
 
@@ -53,3 +58,97 @@ toggleButton.addEventListener("click", () => {
     enableDarkMode();
   }
 });
+
+//********** form **********//
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  username = searchInput.value;
+  getUser(username);
+});
+
+const getUser = async (username) => {
+  try {
+    const response = await fetch("https://api.github.com/users/" + username);
+    if (response.ok) {
+      const data = await response.json();
+      userContainer.setAttribute("style", "display: block");
+      noResult.setAttribute("style", "display: none");
+      updateInfo(data);
+    } else {
+      userContainer.setAttribute("style", "display: none");
+      noResult.setAttribute("style", "display: block");
+    }
+  } catch (error) {
+    userContainer.setAttribute("style", "display: none");
+    console.log("An error occurred:", error);
+  }
+};
+
+const updateInfo = (data) => {
+  // 1. get info
+
+  const avatar = data.avatar_url;
+  const name = data.name;
+  const username = data.login;
+  const dateString = data.created_at;
+  const bio = data.bio;
+  const repos = data.public_repos;
+  const followers = data.followers;
+  const following = data.following;
+  const location = data.location;
+  const company = data.company;
+  const blog = data.blog;
+  const twitter = data.twitter_username;
+
+  //   change date format
+  const date = new Date(dateString);
+  const options = { day: "numeric", month: "short", year: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  // 2. update info
+  document.querySelector(".user-image").src = avatar;
+  document.getElementById("username").textContent = name;
+  document.getElementById("handle").textContent = "@" + username;
+  document.getElementById("date").textContent = formattedDate;
+  document.getElementById("bio").textContent = bio;
+  document.getElementById("repos-value").textContent = repos;
+  document.getElementById("followers-value").textContent = followers;
+  document.getElementById("following-value").textContent = following;
+
+  const locationValue = document.getElementById("location-value");
+  const blogValue = document.getElementById("blog-value");
+  const twitterValue = document.getElementById("twitter-value");
+  const companyValue = document.getElementById("company-value");
+
+  if (location === null || location === "") {
+    locationValue.textContent = "Not Available";
+    locationValue.classList.add("not-available");
+  } else {
+    locationValue.textContent = location;
+    locationValue.classList.remove("not-available");
+  }
+
+  if (blog === null || blog === "") {
+    blogValue.textContent = "Not Available";
+    blogValue.classList.add("not-available");
+  } else {
+    blogValue.textContent = blog;
+    blogValue.classList.remove("not-available");
+  }
+
+  if (twitter === null || twitter === "") {
+    twitterValue.textContent = "Not Available";
+    twitterValue.classList.add("not-available");
+  } else {
+    twitterValue.textContent = twitter;
+    twitterValue.classList.remove("not-available");
+  }
+
+  if (company === null || company === "") {
+    companyValue.textContent = "Not Available";
+    companyValue.classList.add("not-available");
+  } else {
+    companyValue.textContent = company;
+    companyValue.classList.remove("not-available");
+  }
+};
