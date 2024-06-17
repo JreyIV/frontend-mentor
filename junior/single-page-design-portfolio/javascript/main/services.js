f350fc01a4e7f90699da32fd602ee2b5aec9279f
Bruno Moleta <brunomoleta@pm.me>
2024-06-17 17:15:44 -0300

    :bug: fix: change heading level

--- junior/single-page-design-portfolio/javascript/main/services.js
+++ junior/single-page-design-portfolio/javascript/main/services.js
@@ -3,27 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
   if (services) {
     services.innerHTML = `<div class="service-card graphic-design">
     <img src="../../assets/pattern-graphic-design.svg" alt="graphic design" />
-    <h2>Graphic Design</h2>
+    <h3>Graphic Design</h3>
 </div>
 <div class="service-card ui-ux">
     <img src="../../assets/pattern-ui-ux.svg" alt="ui/ux" />
-    <h2>UI/UX</h2>
+    <h3>UI/UX</h3>
 </div>
 <div class="service-card apps">
     <img src="../../assets/pattern-apps.svg" alt="apps" />
-    <h2>Apps</h2>
+    <h3>Apps</h3>
 </div>
 <div class="service-card photography">
     <img src="../../assets/pattern-photography.svg" alt="photography" />
-    <h2>Photography</h2>
+    <h3>Photography</h3>
 </div>
 <div class="service-card illustrations">
     <img src="../../assets/pattern-illustrations.svg" alt="illustrations" />
-    <h2>Illustrations</h2>
+    <h3>Illustrations</h3>
 </div>
 <div class="service-card motion-graphics">
     <img src="../../assets/pattern-motion-graphics.svg" alt="motion graphics" />
-    <h2>Motion Graphics</h2>
+    <h3>Motion Graphics</h3>
 </div>`;
   }
 });
